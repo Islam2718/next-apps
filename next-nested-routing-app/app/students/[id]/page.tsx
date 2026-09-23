0b41270367ee844015959267c0e1168a2533650f
Islam Hossain <mislamhn@gmail.com>
2026-09-23 21:04:55 +0600

    next nested routing app created

---
+++ next-nested-routing-app/app/students/[id]/page.tsx
@@ -0,0 +1,19 @@
+type StudentPageProps = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+export default async function StudentPage({
+  params,
+}: StudentPageProps) {
+  const { id } = await params;
+
+  return (
+    <div>
+      <h1>Student Details</h1>
+
+      <p>Student ID: {id}</p>
+    </div>
+  );
+}
---
+++ next-nested-routing-app/app/students/add/page.tsx
@@ -0,0 +1,7 @@
+export default function AddStudent() {
+    return (
+        <>
+            <h1>Add Student</h1>
+        </>
+    )
+}
